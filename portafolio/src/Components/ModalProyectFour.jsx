cfa84236436d964389f0246c45361819710d232b
valeriaconstanzabc <valeria.brito.c@gmail.com>
2020-08-12 18:29:11 -0400

    proyecto cuatro implementado

--- portafolio/src/Components/ModalProyectFour.jsx
+++ portafolio/src/Components/ModalProyectFour.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import useModal from 'react-hooks-use-modal';
+import '../Css/Modals.css'
  
 export const  ModalOneProyectFour = () => {
   const [Modal, open, close] = useModal('root', {
@@ -11,10 +12,26 @@ export const  ModalOneProyectFour = () => {
             <img className="imgProyectFour3" src="https://i.ibb.co/z75nXXG/Group-1.png" alt=""/> 
         </button>
         <Modal>
-            <div>
-                <h1>Probando</h1>
-                <p>Holiiiiii</p>
-                <button onClick={close}>CLOSE</button>
+            <div className="backgroundModal">
+                <div className="containerTittleAndX">
+                    <h3>Mesero/a debe poder tomar pedido de cliente</h3>
+                    <button class="btnClose" onClick={close}>X</button>
+                </div>
+                <p>
+                    Yo como mesero/a quiero tomar el pedido de un cliente para no 
+                    depender de mi mala memoria, para saber cuánto cobrar, y enviarlo 
+                    a la cocina para evitar errores y que se puedan ir preparando en 
+                    orden.
+                </p>
+                <h4>Criterios de aceptación</h4>
+                <p>
+                    * Anotar nombre de cliente.<br/>
+                    * Agregar productos al pedido.<br/>
+                    * Eliminar productos.<br/>
+                    * Ver resumen y el total de la compra.<br/>
+                    * Enviar pedido a cocina (guardar en alguna base de datos).<br/>
+                    * Se ve y funciona bien en una tablet
+                </p>
             </div>
         </Modal>
     </div>
@@ -32,10 +49,23 @@ export const ModalTwoProyectFour = () => {
                 <img className="imgProyectFour3" src="https://i.ibb.co/XtdxZzZ/Group-3.png" alt=""/>               
             </button>
           <Modal>
-              <div>
-                  <h1>Lo logré?</h1>
-                  <p>Si sis si sis is isisi?</p>
-                  <button onClick={close}>CLOSE</button>
+              <div className="backgroundModal">
+                <div className="containerTittleAndX">
+                    <h3>Jefe de cocina debe ver los pedidos</h3>
+                    <button class="btnClose" onClick={close}>X</button>
+                </div>
+                <p>
+                    Yo como jefe/a de cocina quiero ver los pedidos de los 
+                    clientes en orden y marcar cuáles están listos para 
+                    saber qué se debe cocinar y avisar a los/as meseros/as 
+                    que un pedido está listo para servirlo a un cliente.
+                </p>
+                <h4>Criterios de aceptación</h4>
+                <p>
+                    * Ver los pedidos ordenados según se van haciendo.<br/>
+                    * Marcar los pedidos que se han preparado y están listos para servirse.<br/>
+                    * Ver el tiempo que tomó prepara el pedido desde que llegó hasta que se marcó como completado.
+                </p>
               </div>
           </Modal>
       </div>
@@ -53,10 +83,21 @@ export const ModalThreeProyectFour = () => {
                 <img className="imgProyectFour3" src="https://i.ibb.co/qWYRgRT/Group-2.png" alt=""/>
             </button> 
           <Modal>
-              <div>
-                  <h1>Logradooo</h1>
-                  <p>Eh eh eh eh eh eh</p>
-                  <button onClick={close}>CLOSE</button>
+          <div className="backgroundModal">
+                <div className="containerTittleAndX">
+                    <h3>Mesero/a debe ver pedidos listos para servir</h3>
+                    <button class="btnClose" onClick={close}>X</button>
+                </div>
+                <p>
+                    Yo como mesero/a quiero ver los pedidos que están 
+                    preparados para entregarlos rápidamente a los clientes 
+                    que las hicieron.
+                </p>
+                <h4>Criterios de aceptación</h4>
+                <p>
+                    * Ver listado de pedido listos para servir.<br/>
+                    * Marcar pedidos que han sido entregados.
+                </p>
               </div>
           </Modal>
       </div>
--- portafolio/src/Components/ProyectFour.jsx
+++ portafolio/src/Components/ProyectFour.jsx
@@ -8,95 +8,126 @@ const ProyectFour = () => {
     return (
         <div id="containerProyectFour">
             <div className="containerTittleProyectFour">
-                <h5 className="tittleProyectFourr">BURGER-QUEEN</h5>
-                <h3 className="pointTittleProyectFour">.</h3>
+            <h5 className="tittleProyectFourr">BURGER-QUEEN</h5>
+            <h3 className="pointTittleProyectFour">.</h3>
             </div>
 
-            <div id="containerColorAboutMeProyectFour">
-                <div id="descriptionAboutMeProyectFour1">
+            <div id="containerColorProyectFour">
+                <div id="containerDescriptionProductProyect4">
                     <p id="tProduction">Tiempo de producción: 4 semanas</p>
                     <h2>DEFINICIÓN DE PRODUCTO</h2>
-                    <p>BurgerQueen es un pequeño restaurante de hamburguesas estilo <strong>vintage. </strong> 
-                            Este restaurante está creciendo, por lo que fue necesario realizar una 
-                            interfaz en la que se puedan tomar pedidos usando una <strong>tablet. </strong> Estos 
-                            pedidos posteriormente podrán ser enviados a la cocina, permitiendo 
-                            que la orden sea preparada eficientemente.
-                    </p>
 
                     <div className="container1ProyectFour1">
                         <p className="text1">
-                            <br/>
-                            Esta aplicación posee dos vistas, las cuales se sincronizan en tiempo 
-                            real. La primera vista corresponde al mesero. Este podrá interactuar 
-                            con la app pudiendo ingresar en primera instancia su nombre, la mesa 
-                            a la cual se está atendiendo y el nombre del cliente. Posteriormente 
-                            será redirigido a una nueva orden en donde podrá ingresar los productos 
-                            requeridos, los cuales se irán sumando y acumulando en la boleta. 
-                            Finalmente, la boleta se despachará a la cocina con el botón <strong>"enviar"</strong>, 
-                            y el mesero podrá ver su estado <strong>(Listo, o entregado).</strong> 
-                            <br/>
-                            <br/>
-                            La segunda vista corresponde a la del cocinero o chef. Sin necesidad de 
-                            ingresar un usuario, el chef podrá ingresar directamente al apartado de 
-                            cocina, en donde podrá visualizar todas las ordenes pendientes, pudiendo 
-                            marcarlas como <strong>"listas"</strong> (las ordenes que ya han sido realizadas).
+                        BurgerQueen es un pequeño restaurante de hamburguesas estilo <strong>vintage. </strong> 
+                        Este restaurante está creciendo, por lo que fue necesario realizar una 
+                        interfaz en la que se puedan tomar pedidos usando una <strong>tablet. </strong> Estos 
+                        pedidos posteriormente podrán ser enviados a la cocina, permitiendo 
+                        que la orden sea preparada eficientemente.
+                        <br/>
+                        Esta aplicación posee dos vistas, las cuales se sincronizan en tiempo 
+                        real. La primera vista corresponde al mesero, el cual podrá interactuar 
+                        con la app pudiendo ingresar su nombre, la mesa 
+                        a la cual se está atendiendo y el nombre del cliente. Posteriormente 
+                        será redirigido a una nueva orden en donde se irán acumulando los 
+                        productos requeridos en la boleta. 
+                        Finalmente, la boleta se despachará a la cocina con el botón <strong>"enviar"</strong>, 
+                        y el mesero podrá ver su estado <strong>(Listo, o entregado).</strong> 
+                        <br/>
+                        <br/>
+                        La segunda vista corresponde a la del cocinero o chef. Sin necesidad de 
+                        ingresar un usuario, el chef podrá ingresar directamente al apartado de 
+                        cocina, en donde podrá visualizar todas las ordenes pendientes, pudiendo 
+                        marcarlas como <strong>"listas"</strong> (las ordenes que ya han sido realizadas).
                         </p>
                         <div>
-                            <img className="imgProyectFour1" src="https://i.ibb.co/BLJj4d4/gKFHiM2.png" alt=""/>
+                        <img className="imgProyectFour1" src="https://i.ibb.co/BLJj4d4/gKFHiM2.png" alt=""/>
                         </div>
                     </div>                   
                 </div>
 
-                <div id="descriptionAboutMeProyectFour2">
+                <div id="containerLinksProyect4">
+                    <a target="blank" href="https://burger-queen-2bad6.web.app/">
+                        <button className="btnLinkProyect4">
+                            <img className="imgLinkProyect4" src="https://i.ibb.co/zV5SjV3/Group-7.png" alt=""/>
+                        </button>
+                    </a>
+                    <a target="blank" href="https://github.com/valeriaconstanzabc/SCL013-burger-queen">
+                        <button className="btnGitHubProyect4">
+                            <img className="imgGitHubProyect4" src="https://i.ibb.co/QddgN9p/Group-8.png" alt=""/>
+                        </button>
+                    </a>
+                </div>
+
+                <div id="containerUserDefinition">
                     <h2>DEFINICIÓN DE USUARIO</h2>
-
-                    <div className="container1ProyectFour2">
-                        <div>
-                            <img className="imgProyectFour2" src="http://imgfz.com/i/NqGgDFi.jpeg" alt=""/>
-                        </div>
-
-                        <p className="text2">
-                            <br/>
-                            Para definir a nuestros usuarios, fue necesario tomar en cuenta la 
-                            identidad de BurgerQueen y de sus propios trabajadores.
-                            <br/>
-                            <br/>
-                            Principalmente 
-                            nos centramos en meseros y cocineros a los que el tiempo los apremia 
-                            y que necesitan simplificar y acelerar lo máximo posible sus ordenes. 
-                            <br/>
-                            <br/>
-                            Es por esto que nuestro diseño se basó principalmente y sus necesidades, 
-                            creando una app simple, intuitiva, con botones grandes y vistosos que 
-                            facilitara toda interacción que se realice en ella.
-                        </p>
-                    </div>                   
+                    <p>
+                        Para definir a nuestros usuarios, fue necesario tomar en 
+                        cuenta la identidad de BurgerQueen y de sus propios 
+                        trabajadores. Principalmente nos centramos en meseros y 
+                        cocineros a los que el tiempo los apremia y que necesitan 
+                        simplificar y acelerar lo máximo posible sus ordenes. Es por 
+                        esto que nuestro diseño se basó principalmente y sus 
+                        necesidades, creando una app simple, intuitiva, con botones 
+                        grandes y vistosos que facilitara toda interacción que se 
+                        realice en ella.
+                    </p>
+                    <img className="imgProyectFour2" src="http://imgfz.com/i/NqGgDFi.jpeg" alt=""/>                   
                 </div>
 
-                <div id="descriptionAboutMeProyectFour3">
+                <div id="containerHistoryUserProyect4">
                     <h2 className="text3">HISTORIAS DE USUARIO</h2>
-
                     <div className="container1ProyectFour3">
-                        <div>
-                            { <ModalOneProyectFour />}
-                        </div>
+                        <div>{<ModalOneProyectFour />}</div>
 
                         <div className="containerImgProyectFourArrow3">
                             <img className="imgProyectFourArrow3" src="https://i.ibb.co/TkX1RRV/eva-arrow-ios-forward-fill.png" alt=""/>
                         </div>
 
-                        <div>
-                            { <ModalTwoProyectFour /> }
-                        </div>
+                        <div>{<ModalTwoProyectFour />}</div>
 
                         <div className="containerImgProyectFourArrow3">
                             <img className="imgProyectFourArrow3" src="https://i.ibb.co/TkX1RRV/eva-arrow-ios-forward-fill.png" alt=""/>
                         </div>
 
-                        <div>
-                            { <ModalThreeProyectFour /> }
-                        </div>
+                        <div>{<ModalThreeProyectFour />}</div>
+                    </div>
+                </div>
 
+                <div id="containerToolsProyect4">
+                    <h2>HERRAMIENTAS UTILIZADAS</h2>
+                    <div className="toolsProyectFour">
+                        <div className="imgToolsHTML5Proyect4">
+                            <div className="containerImgToolsProyect4">
+                                <h2 className="textImgToolsProyect4">HTML5</h2>
+                            </div>
+                        </div>
+                        <div className="imgToolsJSProyect4">
+                            <div className="containerImgToolsProyect4">
+                                <h2 className="textImgToolsProyect4">JavaScript</h2>
+                            </div>
+                        </div>
+                        <div className="imgToolsCSSProyect4">
+                            <div className="containerImgToolsProyect4">
+                                <h2 className="textImgToolsProyect4">CSS</h2>
+                            </div>
+                        </div>
+                        <div className="imgToolsReactProyect4">
+                            <div className="containerImgToolsProyect4">
+                                <h2 className="textImgToolsProyect4">React</h2>
+                            </div>
+                        </div>
+                        <div className="imgToolsFigmaProyect4">
+                            <div className="containerImgToolsProyect4">
+                                <h2 className="textImgToolsProyect4">Figma</h2>
+                            </div>
+                        </div>
+                        <div className="imgToolsInvisionProyect4">
+                            <div className="containerImgToolsProyect4">
+                                <h2 className="textImgToolsProyect4">Invision</h2>
+                            </div>
+                        </div>
+                        
                     </div>                   
                 </div>
             </div>            
