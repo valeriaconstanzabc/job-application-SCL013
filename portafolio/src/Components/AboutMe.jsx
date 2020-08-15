37c7fda247cbde18a0c74e54848c482d643d84f0
valeriaconstanzabc <valeria.brito.c@gmail.com>
2020-08-14 20:14:24 -0400

    acerca de mi, terminado

--- portafolio/src/Components/AboutMe.jsx
+++ portafolio/src/Components/AboutMe.jsx
@@ -9,7 +9,23 @@ const AboutMe = () => {
                 <h3 className="pointTittle">.</h3>
             </div>
             <div id="containerColorAboutMe">
-                <p id="descriptionAboutMe">Mi nombre es Valeria...</p>
+                <p id="descriptionAboutMe">
+                    Mi nombre es Valeria Brito, mujer creativa, apasionada por las artes 
+                    manuales, la fotografía, la tecnología y el diseño.
+                    <br/>
+                    <br/>
+                    Este último ha estado presente a lo largo de toda mi vida, y a pesar 
+                    de cursar la carrera de Diseño de ambientes con distinción máxima, lo 
+                    que de verdad llamaba mi atención eran las nuevas tecnologías que aprendí 
+                    durante esos 4 años de estudio.
+                    <br/>
+                    <br/>
+                    Mis capacidades eran mucho mayores y quise arriesgar todo lo que tenía, 
+                    para aceptar el desafío de Laboratoria. Un lugar donde podría complementar 
+                    ambas pasiones y desarrollar grandes talentos como las competencias 
+                    digitales, el autoaprendizaje, la adaptación, y reforzar aún más la 
+                    colaboración en equipo. 
+                </p>
             </div>
             
         </div>
--- portafolio/src/Components/Menu.jsx
+++ portafolio/src/Components/Menu.jsx
@@ -4,35 +4,34 @@ import '../Css/Menu.css'
 
 export default props => {
   return (
-    // Pass on our props
-    <Menu {...props}>
-      <a className="menu-item" href="/">
-        HOME
-      </a>
+      <Menu {...props}>
+        <a className="menu-item" href="/">
+          HOME
+        </a>
 
-      <a className="menu-item" href="/acercaDeMi">
-        ACERCA DE MI
-      </a>
+        <a className="menu-item" href="/acercaDeMi">
+          ACERCA DE MI
+        </a>
 
-      <a className="menu-item" href="/proyectos">
-        PROYECTOS
-      </a>
-      <a className="menu-item-proyects" href="/lofche">
-        - Lofche
-      </a>
-      <a className="menu-item-proyects" href="/guiamuggle">
-        - Guia muggle
-      </a>
-      <a className="menu-item-proyects" href="/gatera">
-        - La gatera hotelera
-      </a>
-      <a className="menu-item-proyects" href="/burgerqueen">
-        - Burguer-queen
-      </a>
+        <a className="menu-item" href="/proyectos">
+          PROYECTOS
+        </a>
+        <a className="menu-item-proyects" href="/lofche">
+          - Lofche
+        </a>
+        <a className="menu-item-proyects" href="/guiamuggle">
+          - Guia muggle
+        </a>
+        <a className="menu-item-proyects" href="/gatera">
+          - La gatera hotelera
+        </a>
+        <a className="menu-item-proyects" href="/burgerqueen">
+          - Burguer-queen
+        </a>
 
-      <a className="menu-item" href="/herramientas">
-        HERRAMIENTAS
-      </a>
-    </Menu>
+        <a className="menu-item" href="/herramientas">
+          HERRAMIENTAS
+        </a>
+      </Menu>
   );
 };
